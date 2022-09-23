ea2edd6858b790dc5abe7eb558bc62978f305673
omarbassatni <omarbassatni@gmail.com>
2022-09-23 23:12:56 +0300

    finished

--- src/components/App.js
+++ src/components/App.js
@@ -5,11 +5,21 @@ import QuestionList from "./QuestionList";
 
 function App() {
   const [page, setPage] = useState("List");
-
+  const [questions, setQuestions] = useState([])
+const handleNewQuestion = (newQuestion) => {
+setQuestions ([...questions,newQuestion])
+}
+const handleDeleteQuestion = (id) => {
+  const updatedQuestions = questions.filter((question) => {
+    if(id !== question.id )
+    return question
+  })
+  setQuestions(updatedQuestions)
+}
   return (
     <main>
       <AdminNavBar onChangePage={setPage} />
-      {page === "Form" ? <QuestionForm /> : <QuestionList />}
+      {page === "Form" ? <QuestionForm handleNewQuestion={handleNewQuestion} /> : <QuestionList handleDeleteQuestion={handleDeleteQuestion} setQuestions={setQuestions} questions={questions}/>}
     </main>
   );
 }
--- src/components/QuestionList.js
+++ src/components/QuestionList.js
@@ -1,10 +1,22 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import QuestionItem from "./QuestionItem";
 
-function QuestionList() {
+function QuestionList({setQuestions, questions,handleDeleteQuestion}) {
+
+  useEffect(() => {
+    fetch("http://localhost:4000/questions")
+      .then((r) => r.json())
+      .then((items) => setQuestions(items));
+  }, []);
+  
   return (
     <section>
       <h1>Quiz Questions</h1>
-      <ul>{/* display QuestionItem components here after fetching */}</ul>
+      <ul>{questions.map(question => {
+        return( 
+          <QuestionItem key={question.id} question={question} handleDeleteQuestion={handleDeleteQuestion}/>
+        )
+      })}</ul>
     </section>
   );
 }
